ec9f66f429df2809c8b759c9774b515d8b991622
MburuRionge <victormburu3@gmail.com>
2024-10-26 00:52:29 +0000

    log passing standard input

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -12,31 +12,31 @@ def print_metrics(code, size):
         if code[i] != 0:
             print("{}: {:d}".format(i, code[i]))
 
-    accepted_code = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+accepted_code = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
 
-    count = 0
-    size = 0
+count = 0
+size = 0
 
-    try:
-        for line in sys.stdin:
-            if count != 0 and count % 10 == 0:
-                print_metrics(accepted_code, size)
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            print_metrics(accepted_code, size)
 
-            parts = line.split()
-            count += 1
+        parts = line.split()
+        count += 1
 
-            try:
-                size += int(parts[-1])
-            except:
-                pass
+        try:
+            size += int(parts[-1])
+        except ValueError:
+            pass
 
-            try:
-                if parts[-2] in accepted_code:
-                    accepted_code[parts[-2]] += 1
-            except:
-                pass
-        print_metrics(accepted_code, size)
+        try:
+            if parts[-2] in accepted_code:
+                accepted_code[parts[-2]] += 1
+        except IndexError:
+            pass
+    print_metrics(accepted_code, size)
 
-    except Keyboardinterrupt:
-        print_metrics(accepted_code, size)
-        raise
+except KeyboardInterrupt:
+    print_metrics(accepted_code, size)
+    raise
