2a3a2ca9ec872a49133d24f98b43b839a30b6f97
MburuRionge <victormburu3@gmail.com>
2024-11-24 10:26:54 +0000

    Write a script that prints all characters of a Star Wars movie:

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,16 +2,39 @@
 
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function[2], function (err, res, body) {
-	(err) throw err;
-	const filmakers = JSON.parse(body).characters;
-	exactOrder(filmakers, 0);
+const filmId = process.argv[2];
+if (!filmId) {
+	console.error('Usage: ./0-starwars_characters.js <film_id>');
+	process.exit(1);
+}
+
+// fwtch fim details by id
+request(`https://swapi-api.hbtn.io/api/films/${filmId}`, (err, res, body) => {
+	if (err) {
+		console.error(err);
+		process.exit(1);
+	}
+
+	try {
+		const characters = JSON.parse(body).characters;
+		fetchCharacters(characters, 0);
+	} catch (error) {
+		console.error('Error parsing JSON:', error);
+		process.exit(1);
+	}
 });
-const exactOrder = (filmakers, x) => {
-	if (x == filmakers.length) return;
-	request(filmakers[x], function (err, res, body) {
-		if (err) throw err;
-		console.log(JSON.parse(body).name);
-		exactOrder(filmakers, x + 1);
+
+//recursive function to fetch characters in order
+const fetchCharacters[index], (err, res, body) => {
+	if (err) {
+		console.error{err};
+		return;
+	}
+
+	try {
+		const name = JSON.parse(body).name;
+		console.log(name);
+	} catch (error) {
+		console.error(characters, index _ 1);
 	});
 };
